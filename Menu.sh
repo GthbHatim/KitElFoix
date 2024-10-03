a0df8dd89451e7ce4ffd7e2022a949a3a6d8441a
Hatim El Housni Lagrine <96115049+GthbHatim@users.noreply.github.com>
2024-10-04 00:39:33 +0200

    Update Menu.sh

--- Menu.sh
+++ Menu.sh
@@ -2,6 +2,7 @@
 #Menu de opciones inicial
 
 clear
+opcion=0
 
 while [ $opcion -ne 5 ] ; do
         echo "1. Esenciales"
@@ -9,7 +10,7 @@ while [ $opcion -ne 5 ] ; do
         echo "3. Programas"
         echo "4. Otros"
         echo "5. Salir"
-        read -p "Selecciona una opcion" opcion
+        read -p "Selecciona una opcion: " opcion
 
         case "$opcion" in
         1) sudo bash ./recursos/esenc.sh 
@@ -21,6 +22,7 @@ while [ $opcion -ne 5 ] ; do
         4) sudo bash ./recursos/otros.sh 
         ;;
         5) echo "Cerrando."
+           exit 1     
         ;;
         *) echo "Invalido, comando inexistente."
                 opcion=0 ;;
