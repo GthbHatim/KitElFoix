dd7eecaa42c7b4732a1b43e9e566eb5a810ada88
Hatim El Housni Lagrine <96115049+GthbHatim@users.noreply.github.com>
2024-10-04 00:28:39 +0200

    Update Menu.sh

--- Menu.sh
+++ Menu.sh
@@ -3,25 +3,27 @@
 
 clear
 
-opcion=0
-
-while [$opcion -ne 5]; do
+while [ $opcion -ne 5 ] ; do
         echo "1. Esenciales"
         echo "2. Juegos"
         echo "3. Programas"
         echo "4. Otros"
         echo "5. Salir"
-        read opcion
+        read -p "Selecciona una opcion" opcion
 
-        case $opcion
-        1) sudo bash ./recursos/esenc.sh ;;
-        2) sudo bash ./recursos/juegos.sh ;;
-        3) sudo bash ./recursos/programas.sh ;;
-        4) sudo bash ./recursos/otros.sh ;;
+        case "$opcion" in
+        1) sudo bash ./recursos/esenc.sh 
+        ;;
+        2) sudo bash ./recursos/juegos.sh 
+        ;;
+        3) sudo bash ./recursos/programas.sh 
+        ;;
+        4) sudo bash ./recursos/otros.sh 
+        ;;
         5) echo "Cerrando."
+        ;;
         *) echo "Invalido, comando inexistente."
-                opcion=0
-                ;;
+                opcion=0 ;;
         esac
         sleep 5s
 done
