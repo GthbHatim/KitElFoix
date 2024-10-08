726048bd80b1486bcf74f139dda69895d8abc4d0
Hatim El Housni Lagrine <96115049+GthbHatim@users.noreply.github.com>
2024-10-08 23:28:22 +0200

    .

--- recursos/esenc.sh
+++ recursos/esenc.sh
@@ -22,6 +22,7 @@ sleep 5s
     #Instalando Wine (x64 y x32)
     sudo dpkg --add-architecture i386
     sudo mkdir -pm755 /etc/apt/keyrings
+    sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
     sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists/bookworm/winehq-bookworm.sources
     sudo apt update -y
-    sudo apt install --install-recommends winehq-stable -yg
+    sudo apt install --install-recommends winehq-stable -y
